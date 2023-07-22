0a29c4c16b5788484fe019dd68786214a7dee693
theGeekkit <sonofgondolin16@gmail.com>
2023-07-21 20:22:03 -0500

    cookie

---
+++ who ate cookiees.js
@@ -0,0 +1,26 @@
+function cookie(x){
+ 
+    if (typeof x === "string") {
+        return "Who ate the last cookie? It was Zach!";
+    }
+    else if (typeof x === "number") {
+        return "Who ate the last cookie? It was Monica!"
+    }
+    else {
+        return "Who ate the last cookie? It was the dog!";
+    }
+
+}
+
+
+
+  describe("Tests", () => {
+    it("test", () => {
+  Test.assertEquals(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
+  Test.assertEquals(cookie(26), "Who ate the last cookie? It was Monica!")
+  Test.assertEquals(cookie(2.3), "Who ate the last cookie? It was Monica!")
+  Test.assertEquals(cookie(true), "Who ate the last cookie? It was the dog!")
+  
+    });
+  });
+  
