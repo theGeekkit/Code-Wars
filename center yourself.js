7563d0669792eb5241ad34f8554cdcd4fc130201
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-07-24 18:27:39 -0500

    c2

--- center yourself.js
+++ center yourself.js
@@ -23,3 +23,17 @@ describe("Tests suite", function() {
     doTest(["abcdefg", 2, ' '], "abcdefg");
   });
 });
+
+
+
+// Implement a function center that takes a string strng, an integer width, and an optional character fill (default: ' ') and returns a new string of length width where strng is centered and on the right and left padded with fill.
+
+// center(strng, width, fill=' ')
+// If the left and right padding cannot be of equal length make the padding on the left side one character longer.
+
+// If strng is longer than width return strng unchanged.
+
+// Examples
+// center('a', 3)  # returns " a "
+// center('abc', 10, '_')  # returns "____abc___"
+// center('abcdefg', 2)  # returns "abcdefg"
